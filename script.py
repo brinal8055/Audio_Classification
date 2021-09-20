0b4f63def4470e41997b3f388c1fb8e05b8ed448
brinal8055 <brinalsavsaviya@gmail.com>
2021-09-20 19:00:11 +0530

    Test file added

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ import os
 a = 'MLP' if Classification_model.xif == '1' else 'CNN'
 print('Classification using',a,'\n')
 
-for _ in os.listdir("./test"):
+for d in os.listdir("./test"):
     print('Output of the file ' + _ + ' is:')
-    Classification_model.print_prediction(_)
+    Classification_model.print_prediction(d)
