69397636bf6d21839a65142015d60d12707c7085
brinal8055 <brinalsavsaviya@gmail.com>
2021-10-21 17:09:33 +0530

    Using Sounddevice library

--- audio_reading.py
+++ audio_reading.py
@@ -1,38 +1,44 @@
-import pyaudio
-import wave
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
 
-p = pyaudio.PyAudio()
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+# import pyaudio
+# import wave
 
-print("Recording started")
+# CHUNK = 1024
+# FORMAT = pyaudio.paInt16
+# CHANNELS = 2
+# RATE = 44100
+# RECORD_SECONDS = 5
+# WAVE_OUTPUT_FILENAME = "output.wav"
 
-frames = []
+# p = pyaudio.PyAudio()
 
-for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+# stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-print("Recorded")
+# print("Recording started")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+# frames = []
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+# for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
+#     data = stream.read(CHUNK)
+#     frames.append(data)
+
+# print("Recorded")
+
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
+
+# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+# wf.setnchannels(CHANNELS)
+# wf.setsampwidth(p.get_sample_size(FORMAT))
+# wf.setframerate(RATE)
+# wf.writeframes(b''.join(frames))
+# wf.close()
